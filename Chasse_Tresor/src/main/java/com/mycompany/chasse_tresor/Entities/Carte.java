fe1feba3c4747c0a8d3ac571627a535cac88b5b9
SARDA <baptiste.sarda@gmail.com>
2016-11-28 12:29:49 +0100

    Création package

---
+++ Chasse_Tresor/src/main/java/com/mycompany/chasse_tresor/Entities/Carte.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chasse_tresor.Entities;
+
+/**
+ *
+ * @author flesguer
+ */
+public class Carte {
+
+    public int largeur;
+    public int hauteur;
+
+    public Carte(int largeur, int hauteur) {
+        
+        
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+    }
+    
+}
--- Chasse_Tresor/src/main/java/com/mycompany/chasse_tresor/Partie/Partie.java
+++ Chasse_Tresor/src/main/java/com/mycompany/chasse_tresor/Partie/Partie.java
@@ -1,4 +1,4 @@
-package com.mycompany.chasse_tresor;
+package com.mycompany.chasse_tresor.Partie;
 
 /**
  * Created by flesguer on 27/11/2016.
@@ -8,6 +8,9 @@ package com.mycompany.chasse_tresor;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import com.mycompany.chasse_tresor.Entities.Carte;
+import com.mycompany.chasse_tresor.Util.Ligne;
+
 import java.io.*;
 
 
@@ -21,7 +24,6 @@ public class Partie{
     public static void main(String[] args) {
 
         try{
-            //FileInputStream fstream = new FileInputStream("textfile.txt");
             FileInputStream fstream = new FileInputStream("fichierCarte.txt");
             DataInputStream in = new DataInputStream(fstream);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
@@ -29,11 +31,7 @@ public class Partie{
             while ((strLine = br.readLine()) != null)   {
                 String[] tokens = strLine.split(" ");
 
-
                 Ligne ligne = new Ligne(tokens[0],tokens[1],tokens[2]);//process record , etc
-
-
-
 
                 if (ligne.valeur1.equals("C")){
                        Carte carte = new Carte(Integer.parseInt(ligne.valeur2), Integer.parseInt(ligne.valeur3));
@@ -47,29 +45,17 @@ public class Partie{
                     System.out.println("Il y a "+ligne.valeur3 + " tresor(s) à l'emplacement " +ligne.valeur2);
                 }
 
-
                 System.out.println(ligne.valeur1 + ligne.valeur2 + ligne.valeur3);
 
             }
             in.close();
-        }catch (Exception e){
-            System.err.println("Error: " + e.getMessage());        }
-
-
-
-/*
-    Carte carte = new Carte(ToInt(line.substring(2,1)), ToInt(line.substring(4,1)));
-
-        System.out.println("La carte fait une taille de VAR par VAR");
-        System.out.println("Il y a un tresor à l'emplacement VAR VAR");
-        System.out.println("Il y a un seconde trésor à l'emplacement VAR VAR");
-        System.out.println("Il y a un monstre à l'emplacement VAR VAR");
-*/
-
-}
+        }
+        catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
 
     private static int ToInt(String substring) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
 }
